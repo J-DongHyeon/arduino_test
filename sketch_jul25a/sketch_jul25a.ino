ab11741f75f3bfd1aed3e610e2acba3772bfbfad
DESKTOP-MG12BQR\jdh38 <jdh3876@naver.com>
2021-07-25 22:34:09 +0900

    GPIO_test

--- sketch_jul25a/sketch_jul25a.ino
+++ sketch_jul25a/sketch_jul25a.ino
@@ -1,9 +1,34 @@
-void setup() {
-  // put your setup code here, to run once:
+unsigned int data = 0x03;
 
+void setup() {
+
+ for(int i=8; i<=11; i++) {
+ pinMode(i, OUTPUT);
+ }
+ 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (data & 0x01) {
+    digitalWrite(8, HIGH);
+  } else {
+    digitalWrite(8, LOW);
+  }
+  if (data & 0x02) {
+    digitalWrite(9, HIGH);
+  } else {
+    digitalWrite(9, LOW);
+  }
+  if (data & 0x04) {
+    digitalWrite(10, HIGH);
+  } else {
+    digitalWrite(10, LOW);
+  }
+  if (data & 0x08) {
+    digitalWrite(11, HIGH);
+  } else {
+    digitalWrite(11, LOW);
+  }
+delay(500);
 
 }
