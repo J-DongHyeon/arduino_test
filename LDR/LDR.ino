c385738e8149059fff67b7032fdf0bac5d8f8090
DESKTOP-MG12BQR\jdh38 <jdh3876@naver.com>
2021-08-08 23:26:29 +0900

    end; LDR

--- LDR/LDR.ino
+++ LDR/LDR.ino
@@ -1,9 +1,20 @@
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int data = analogRead(A0);
+
+  if (data < 500) {
+    Serial.println("LIGHT : VERY LOW");
+  } else if (data < 600) {
+    Serial.println("LIGHT : LOW");
+  } else if (data < 850) {
+    Serial.println("LIGHT : HIGH");
+  } else if (data <= 1023) {
+    Serial.println("LIGHT : VERY HIGH");
+  }
 
 }
