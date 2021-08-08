36534278f005f7e362a869383787bc37741bc3ec
DESKTOP-MG12BQR\jdh38 <jdh3876@naver.com>
2021-08-08 22:43:56 +0900

    end; smoothing

--- smoothing/smoothing.ino
+++ smoothing/smoothing.ino
@@ -1,9 +1,37 @@
+
+int array_size = 8;
+float total = 0;
+float avg = 0;
+int now_index = 0;
+float smoothing[8];
+int data;
+float analog_volt;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(A0, INPUT);
+
+  Serial.begin(9600);
+  for (int i =0 ;i<8; i++) {
+    smoothing[i] = 0;
+  }
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  data = analogRead(A0);
+  analog_volt = data * (5 / 1023.0);
+  
+  total -= smoothing[now_index];
+  smoothing[now_index] = analog_volt;
+  total += smoothing[now_index];
 
+  avg = total / array_size;
+
+  Serial.println(avg);
+  
+  now_index++;
+  now_index &= 0x7;
+
+  delay(1);
+  
 }
